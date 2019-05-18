967c3bf8efecc918d36157ac64ffb145ba2396cf
kienbtreal <50767898+kienbtreal@users.noreply.github.com>
2019-05-18 16:53:03 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://www.theparamiresort.com/'>http://www.theparamiresort.com/</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://trungkienreal.com/'>http://trungkienreal.com//</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
